6e1fac154f4d61254672dde658494c26ecbc09b9
Aidan <AidanofVT@gmail.com>
2022-01-29 20:21:28 -0500

    Minimum viable product. Counts up or down, and can have its counting rate dynamically changed. Doesn't have any error-checking. Doesn't have any ability to override the count.

--- myStuff/prototype.cpp
+++ myStuff/prototype.cpp
@@ -3,44 +3,85 @@
 #include <ctime>
 #include <string>
 
-bool run {true};
+// MUST "DEALLOCATE" C-STYLE STRINGS!!!!!
 
-void processCommand (std::string line) {
-    
+bool running {false};
+int anchorTime{time(NULL)};
+int countAsOfLastAnchor{0};
+int countNow{0};
+float multiplier{1};
+int rightwardness{0};
+
+void newStart () {
+    countAsOfLastAnchor = countNow;
+    anchorTime = time(NULL);
+}
+
+std::string takeCommand () {
+    std::string toReturn{};
+    char charInQuestion{};
+    rightwardness = 0;    
+    while ((charInQuestion = mvinch(1, rightwardness)) != ' ') {
+        toReturn += charInQuestion;
+        ++rightwardness;
+        }  
+    rightwardness = 0;
+    mvprintw(1, rightwardness, "");            
+    clrtoeol();
+    return toReturn;
+}
+
+void enactTimeSumChange (std::string timeChange) {}
+
+void enactMultiplierChange (std::string newMultiplier) {
+    switch (newMultiplier[0]) {
+        case 'u':
+            newMultiplier.erase(0,1);
+            break;
+        case 'd':
+            newMultiplier.replace(0, 1, "-");
+            break;
+    }
+    newStart();
+    multiplier = std::stof(newMultiplier);
+}
+
+void processCommand (std::string command) {
+    switch (command[0]) {
+        case 'u':
+        case 'd':
+            enactMultiplierChange(command);
+            break;
+    }
 }
 
 int main () {
     WINDOW *myWindow{initscr()};
     raw();
     nodelay(stdscr, true);
-    bool running{true};
-    int anchorTime{time(NULL)};
-    int sumTime{0};
     int lastCharHit{};
-    int rightwardness{0};
     while (lastCharHit != 'q') {
-        lastCharHit = mvwgetch(stdscr, 1, rightwardness);
-        if (running) {     
-            int toPrint{sumTime + difftime(time(NULL), anchorTime)};    
-            mvprintw(0, 0, "%i", toPrint);
+        lastCharHit = mvgetch(1, rightwardness);
+        if (running) {
+            countNow = countAsOfLastAnchor + difftime(time(NULL), anchorTime) * multiplier;
+            mvprintw(0, 0, "");            
+            clrtoeol();
+            mvprintw(0, 0, "%i", countNow);
+            mvprintw(1, rightwardness,"");
         }
-        if (lastCharHit == ' ') {
+        if (lastCharHit == ' ') {            
             running = !running;
-            if (running == false) {
-                sumTime = sumTime + difftime(time(NULL), anchorTime);                
+            if (running == true) {
+                newStart();               
             }
-            else {
-                anchorTime = time(NULL);
-            }
+            mvprintw(1, rightwardness,"");
         }
-        // if (lastCharHit == 13) {
-        //     rightwardness = 0;
-        //     mvprintw(1, rightwardness, "");            
-        //     clrtoeol();
-        // }
-        // else if (lastCharHit != ERR) {
-        //     mvprintw(1, ++rightwardness - 1, "%c", lastCharHit);
-        // }        
+        else if (lastCharHit == 13) {
+            processCommand(takeCommand());
+        }
+        else if (lastCharHit != ERR) {
+            mvprintw(1, ++rightwardness - 1, "%c", lastCharHit);
+        }      
         refresh();
         Sleep(10);
     }
